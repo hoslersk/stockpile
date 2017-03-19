5eca84c400d64e9a6b2c215c2ccde6800b3ba4e8
Schyler Hosler <schylerhosler@gmail.com>
2017-03-19 15:56:50 -0400

    Initial login form creation

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -4,7 +4,30 @@ export default class LogIn extends Component {
 	render() {
 		return (
 			<div className="page-content">
-				Log In Page
+				<form className="login-form">
+					<div className="form-title">Welcome Back!</div>
+					<div className="form-field">
+						<label className="form-label">Username</label>
+						<input
+							className="form-input"
+							type="text"
+							name="username"
+							required
+						/>
+					</div>
+
+					<div className="form-field">
+						<label className="form-label">Password</label>
+						<input
+							className="form-input"
+							type="password"
+							name="password"
+							required
+						/>
+					</div>
+
+					<button className="form-button" type="submit">Login</button>
+				</form>
 			</div>
 		)
 	}
